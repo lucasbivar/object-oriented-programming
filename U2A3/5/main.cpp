f48b981dbc416334655c54553d1af262537b7f11
lucasbivar <lucasbivarfonseca@gmail.com>
2021-08-19 15:00:04 -0300

    refactor: improving the main program

--- U2A3/5/main.cpp
+++ U2A3/5/main.cpp
@@ -8,20 +8,22 @@ using std::cin;
 int main() {
   Array a1(7);  //array de 7 elementos
   Array a2;     //array de 10 elementos
+
+  // preenchendo os arrays
   for(int i = 0; i < a1.getSize(); i++) a1[i] = i;
   for(int i = 0; i < a2.getSize(); i++) a2[i] = i;
 
-  cout << a1;
-  cout << a2;
+  cout << "Array A1 -> " << a1;
+  cout << "Array A2 -> " << a2;
+
+  cout << endl;
+
   Array a3 = a1 + a2;
-  cout << a3;
-  cout << a1;
-  cout << a2;
+  cout << "Array A3 = Array A1 + Array A2 -> " << a3;
+  cout << endl;
 
   a3 += a1;
-  cout << a3;
-  cout << a1;
-  cout << a2;
+  cout << "Array A3 += Array A1 -> " << a3;
  
   return 0;
 }
