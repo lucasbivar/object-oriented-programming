0deead630175e35bd32e5227c12a969a190c471b
Lucas <lucasbivarfonseca@gmail.com>
2021-09-29 22:19:40 -0300

    test: testing the things implemented

--- OOP-Projects/ATM/src/main.cpp
+++ OOP-Projects/ATM/src/main.cpp
@@ -2,8 +2,15 @@
 using std::cout;
 using std::endl;
 
+#include "../include/Pessoa/Pessoa.h"
+#include "../include/Pessoa/PessoaFisica.h"
+#include "../include/Pessoa/PessoaJuridica.h"
+
 int main(){
-
-
+  Pessoa* arr[2] = {new PessoaFisica("Lucas Bivar", "lucas@gmail.com", "12312", "234233", "dasda ds"),
+  new PessoaJuridica("lUCAS", "123123", "1234", "12312", "FSDFS")};
+  
+  arr[0]->exibirPessoa();
+  arr[1]->exibirPessoa();
   return 0;
 }
